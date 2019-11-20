1f2e36f78ce3d8ddc475d87e7d982ba0d44916c1
Ashim <ashimpandey2015@gmail.com>
2019-11-20 18:02:15 +0530

    Form Error message

--- src/_containers/formview/sf_tf.formview.container.js
+++ src/_containers/formview/sf_tf.formview.container.js
@@ -2,20 +2,39 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Wrap from "../../_components/COMMON/Wrap/_wrap";
 
-const Formview = props =>
-    <div className={`sf-tf-formview ${props.className} ${props.type === 'app' ? ' sf-tf-appview' : ''}`} id={props.id}>
-        {
-            // props.type === 'form'
-                //?
-              <Wrap>
-                <iframe id="typeform-full" width="100%" height="100%" frameBorder="0" src={ props.form }></iframe>
-                <script type={'text/javascript'} src={'https://embed.typeform.com/embed.js'}></script>
-              </Wrap>
-                // :
-            // props.type === 'app'
-            //     ?   null
-            //     :   <iframe id="app" width="100%" height="100%" frameBorder="0" src={ props.form }></iframe>
-        }
-    </div>
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
+const Formview = props => 
+<div className={`sf-tf-formview ${props.className} ${props.type === 'app' ? ' sf-tf-appview' : ''}`} id={props.id}>
+{
+    
+    // props.type === 'form'
+        //?
+
+        (validURL(props.form))
+        ?
+       <Wrap> 
+            <iframe id="typeform-full" width="100%" height="100%" frameBorder="0" src ={props.form}></iframe>
+            <script type={'text/javascript'} src={'https://embed.typeform.com/embed.js'} ></script>
+        </Wrap>
+
+        :
+        <Wrap>No content available</Wrap>
+
+        // :
+    // props.type === 'app'
+    //     ?   null
+    //     :   <iframe id="app" width="100%" height="100%" frameBorder="0" src={ props.form }></iframe>
+}
+</div>
+
 
 export default Formview;
